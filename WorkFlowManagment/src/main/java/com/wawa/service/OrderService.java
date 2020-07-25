18735ea3eef0ca4541f02ae1623346c409a12791
ferozsheik <ferozsheik@localhost>
2020-07-25 13:52:24 -0400

    Project Structure

---
+++ WorkFlowManagment/src/main/java/com/wawa/service/OrderService.java
@@ -0,0 +1,15 @@
+package com.wawa.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Service;
+
+import com.wawa.repository.OrderDelegate;
+
+@Service
+public class OrderService {
+	
+	
+}
