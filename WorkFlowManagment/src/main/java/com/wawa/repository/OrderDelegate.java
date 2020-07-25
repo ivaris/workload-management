e498d45a95e453adf1040781ec19683e2d6d6259
ferozsheik <ferozsheik@localhost>
2020-07-25 13:53:04 -0400

    Delegate updated

--- WorkFlowManagment/src/main/java/com/wawa/repository/OrderDelegate.java
+++ WorkFlowManagment/src/main/java/com/wawa/repository/OrderDelegate.java
@@ -2,10 +2,5 @@ package com.wawa.repository;
 
 public interface OrderDelegate  {
 
-		/*	@Query("SELECT DISTINCT o FROM Order o WHERE s.order_id = (:order_id)")
-		    public Order retrieveByOrderId(@Param("order_id") String orderId);
-			
-			@Query("DELETE FROM Order o WHERE s.order_id = (:order_id)")
-			public void deleteByOrderId(@Param("order_id") String orderId); */
-		}
+}
 
